41bff6e0d4da8193793ddf5f46fbbb9e7bbbccea
alpylmz <ylmz.alp.e@gmail.com>
2022-04-26 11:32:24 +0300

    stockfish now updates fen string itself

--- src/chessmate/scripts/stockfish_driver.py
+++ src/chessmate/scripts/stockfish_driver.py
@@ -3,20 +3,35 @@ import stockfish
 from chessmate.srv import chess_next_move, chess_next_moveResponse
 from chessmate.srv import chess_opponent_move, chess_opponent_moveResponse
 
-# need stockfish binary path!
+
+# need stockfish binary path!   
 _stockfish = stockfish.Stockfish(path="/home/alp/Downloads/stockfish_14.1_linux_x64/stockfish_14.1_linux_x64")
 #_stockfish = stockfish.Stockfish(path="/home/dogukan/Downloads/stockfish_14.1_linux_x64/stockfish_14.1_linux_x64")
+
+
+# Set starting state of the board. 
+INITIAL_FEN_STRING = "1k4N1/8/8/2p1K3/B7/3Pb2N/8/2r5 w - - 0 1"
+_stockfish.set_fen_position(INITIAL_FEN_STRING)
+
+
 
 def chess_next_move_func(req):
     global _stockfish
     best_move = _stockfish.get_best_move()
-    return chess_next_moveResponse(1, best_move[:2], best_move[2:])
+    _stockfish.make_moves_from_current_position(moves=[best_move])
+    return chess_next_moveResponse(1, best_move[:2], best_move[2:],_stockfish.get_fen_position())
+
+
+
 
 def chess_opponent_move_func(req):
     global _stockfish
     opponent_move = req.move
-    _stockfish.set_position(moves=[opponent_move])
-    return chess_opponent_moveResponse()
+    _stockfish.make_moves_from_current_position(moves=[opponent_move])
+    return chess_opponent_moveResponse(_stockfish.get_fen_position())
+
+
+
 
 if __name__ == "__main__":
     rospy.init_node("stockfish_driver")
