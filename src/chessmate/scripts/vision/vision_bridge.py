9823d16ddf1585b148af1f2e565f150de16eb702
BURAK HAN GENCMEHMETOGLU <e2310001@ceng.metu.edu.tr>
2022-05-17 08:26:03 +0000

    Replace vision_bridge.py

--- src/chessmate/scripts/vision/vision_bridge.py
+++ src/chessmate/scripts/vision/vision_bridge.py
@@ -1,27 +1,65 @@
 import rospy
-
-from vision import Vision
+from camera import Camera
+from coordinate import Coordinate
+from top_vision import TopVision
+from side_vision import SideVision
+from face_tracer import FaceTracer
 from chessmate.srv import QueryVisionComponentResponse, QueryVisionComponent, getPositionOfPieces, getPositionOfPiecesResponse
+from return_codes import *
 
+SQUARE_WIDTH = 0.041
 TOP_LEFT_X = 0.5410416700640096
 TOP_LEFT_Y = 0.320580303627435
 
+
 # Since the main loop is in C++, I need to make vision.py a ROS service
 if __name__ == "__main__":
-        
-    vision_system = Vision(TOP_LEFT_X, TOP_LEFT_Y)
+    
+    camera = Camera()
+    coordinate_class = Coordinate(TOP_LEFT_X, TOP_LEFT_Y, SQUARE_WIDTH)
+    top_vision = TopVision(camera)
+    side_vision = SideVision(camera)
+    face_tracer = FaceTracer(camera)
 
     # init ros node
     rospy.init_node('vision_bridge')
 
+
     # queryvisioncomponent handler, executes vision_system.get_movement()
     def queryvisioncomponent_handler(req):
-        return_code, movement_in_fen = vision_system.get_movement(req.last_state_fen_string)
+        if req.query_type == "get_emotion":
+            return_code = face_tracer.get_emotion()
+            return QueryVisionComponentResponse(return_code,"")
+            
+        if req.query_type == "align_face":
+            return_code = face_tracer.is_face_aligned()
+            return QueryVisionComponentResponse(return_code, "")
+
+        if req.query_type == "test":
+            return QueryVisionComponentResponse(1,"")
+
+        if req.query_type == "update_prev":
+            side_vision.update_prev_image()
+            return QueryVisionComponentResponse(1,"")
+
+        if req.query_type == "side":
+            return_code, movement_in_fen = side_vision.get_movement(req.last_state_fen_string)
+
+        if req.query_type == "top":
+            return_code, movement_in_fen = top_vision.get_movement(req.last_state_fen_string)
+
+
+
         return QueryVisionComponentResponse(return_code, movement_in_fen)
 
+
+
+
     def getPieceCoordinates(req):
-        from_coords, to_coords = vision_system.get_piece_coordinates(req.from_piece, req.to_piece)
+        from_coords, to_coords = coordinate_class.get_piece_coordinates(req.from_piece, req.to_piece)
         return getPositionOfPiecesResponse(True, from_coords[0], from_coords[1], to_coords[0], to_coords[1])
+
+
 
     # start a ros service
     rospy.Service('/franka_vision', QueryVisionComponent, queryvisioncomponent_handler)
