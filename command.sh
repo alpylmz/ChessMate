bbd0b1430073276ce3b1a024e1da3bc4cfa432c1
alpylmz <ylmz.alp.e@gmail.com>
2022-05-23 13:32:36 +0300

    the command for all to run

---
+++ command.sh
@@ -0,0 +1,7 @@
+gnome-terminal -e "bash -c 'echo move_to_start && cd ~/franka && source devel/setup.bash && roslaunch franka_example_controllers move_to_start.launch robot_ip:=\"10.0.0.2\" load_gripper:=false rviz:=true && sleep 1000'" &
+gnome-terminal -e "bash -c 'echo stockfish && cd /home/alp/bitirme/ChessMate && source devel/setup.bash && cd /home/alp/bitirme/ChessMate/src/chessmate/scripts && python3 stockfish_driver.py && sleep 1000'" &
+gnome-terminal -e "bash -c 'echo vision && cd /home/alp/bitirme/ChessMate && source devel/setup.bash && cd /home/alp/bitirme/ChessMate/src/chessmate/scripts/vision && python3 vision_bridge.py && sleep 1000'" &
+gnome-terminal -e "bash -c 'echo arduino && cd /home/alp/bitirme/ChessMate && source devel/setup.bash && cd /home/alp/bitirme/ChessMate/src/chessmate/scripts/ && python3 arduino-driver.py && sleep 1000'" &
+gnome-terminal -e "bash -c 'echo chessmate && cd /home/alp/bitirme/ChessMate && source devel/setup.bash && rosrun chessmate chessmate && sleep 1000'" &
+
+gnome-terminal -e "bash -c 'cd ~/franka && source devel/setup.bash && rosrun franka_gripper franka_gripper_service 10.0.0.2 0 0 && sleep 1000'" &
